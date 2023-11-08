cef550d2d5b74d9728887c52d799d75be0ba084e
smartshivam <smartshivam2002@gmail.com>
2023-11-08 23:31:22 +0530

    express setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+import { PORT } from "./constants.js";
 
 connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server is listing on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("server error",err);
+    process.exit()
+  });
